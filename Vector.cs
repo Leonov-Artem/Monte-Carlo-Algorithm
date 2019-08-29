1db0e5ccd7773e4f520bc5336ee2689f6c8b2f27
Artem Leonov <leonovru@yandex.ru>
2019-08-29 20:51:36 +0300

    Finished class Vector

--- Vector.cs
+++ Vector.cs
@@ -6,5 +6,22 @@ namespace MonteCarloAlgorithm
 {
     public class Vector
     {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Vector(Point start, Point end)
+        {
+            X = end.X - start.X;
+            Y = end.Y - start.Y;
+        }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static double PseudoscalarProduct(Vector a, Vector b)
+            => a.X * b.Y - b.X * a.Y;
     }
 }
