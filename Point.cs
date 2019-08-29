a7805f1d1b3ec9589a3f7242374938ff5e568eec
Artem Leonov <leonovru@yandex.ru>
2019-08-29 20:30:57 +0300

    Finished class UniformDistribution

--- Point.cs
+++ Point.cs
@@ -6,5 +6,13 @@ namespace MonteCarloAlgorithm
 {
     public class Point
     {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
--- UniformDistribution.cs
+++ UniformDistribution.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace MonteCarloAlgorithm
 {
-    public class UniformDistribution
+    public static class UniformDistribution
     {
-
+        public static double GetNum(double lower_bound, double upper_bound)
+        {
+            var random = new Random();
+            return random.NextDouble() * (upper_bound - lower_bound + 1) + lower_bound;
+        }
     }
 }
