6eacc3a66246e24f4ebb076d3666d5ed13a439d4
Artem Leonov <leonovru@yandex.ru>
2019-08-29 21:31:03 +0300

    Finished class Rectangle

--- Rectangle.cs
+++ Rectangle.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace MonteCarloAlgorithm
 {
-    public class Rectangle
+    public class Rectangle : IShape
     {
+        private double _minX;
+        private double _maxX;
+        private double _minY;
+        private double _maxY;
+        public double Length { get; private set; }
+        public double Width { get; private set; }
+
+        public Rectangle(Point b, Point d)
+        {
+            _minX = b.X;
+            _maxY = d.Y;
+            _maxX = _minX + (d.X - b.X) + (d.Y - b.Y);
+            _minY = _maxY - 2 * (d.Y - b.Y);
+
+            Length = _maxX - _minX;
+            Width = _maxY - _minY;
+        }
+
+        public double Area()
+            => Length * Width;
+
+        public bool Сontains(Point point)
+            => throw new NotImplementedException();
     }
 }
