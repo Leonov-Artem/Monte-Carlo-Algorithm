22130d6f3cd731308054323c5e6989e854f28110
Artem Leonov <leonovru@yandex.ru>
2019-08-29 19:56:05 +0300

    Add the necessary classes

---
+++ IShape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarloAlgorithm
+{
+    public interface IShape
+    {
+        double Area();
+        double Сontains(Point point);
+    }
+}
---
+++ UniformDistribution.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarloAlgorithm
+{
+    public class UniformDistribution
+    {
+
+    }
+}
