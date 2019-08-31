567fad44d92ec7754dcf073249d6e89ab065c5e3
Artem Leonov <leonovru@yandex.ru>
2019-08-31 21:56:09 +0300

    Added implementation of the method "contains" of the class "triangle"

--- Triangle.cs
+++ Triangle.cs
@@ -8,6 +8,7 @@ namespace MonteCarloAlgorithm
     {
         private Boundaries _boundX;
         private Boundaries _boundY;
+        private Point d, b, e;
         public double Base { get; private set; }
         public double Height { get; private set; }
         public double Area { get; private set; }
@@ -17,11 +18,20 @@ namespace MonteCarloAlgorithm
             Base = 2 * (d.Y - b.Y);
             Height = d.X - b.X;
             Area = 0.5 * Base * Height;
+            this.b = b;
+            this.d = d;
+            this.e = new Point(d.X, d.Y - Base);
             _boundX = new Boundaries(b.X, d.X);
             _boundY = new Boundaries(d.Y - Base, d.Y);
         }
 
         public bool Contains(Point point)
-            => throw new NotImplementedException();
+        {
+            var be = new Vector(b, e);
+            var bd = new Vector(b, d);
+            var bx = new Vector(b, point);
+            return Vector.PseudoscalarProduct(be, bx) >= 0 && 
+                   Vector.PseudoscalarProduct(bx, bd) >= 0;
+        }
     }
 }
--- Vector.cs
+++ Vector.cs
@@ -8,17 +8,20 @@ namespace MonteCarloAlgorithm
     {
         public double X { get; private set; }
         public double Y { get; private set; }
+        public double Length { get; private set; }
 
         public Vector(Point start, Point end)
         {
             X = end.X - start.X;
             Y = end.Y - start.Y;
+            Length = Math.Sqrt(X * X + Y * Y);
         }
 
         public Vector(double x, double y)
         {
             X = x;
             Y = y;
+            Length = Math.Sqrt(X * X + Y * Y);
         }
 
         public static double PseudoscalarProduct(Vector a, Vector b)
